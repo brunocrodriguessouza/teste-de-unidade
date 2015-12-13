1c872f367fcd3b149a19d90d334c78cedb71c4ef
Bruno Souza <brunoprogramadorjava>
2015-12-13 00:05:47 -0200

    Iniciando curso alura de teste unidades

---
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,26 @@
+package br.com.caelum.leilao.teste;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.servico.Avaliador;
+
+public class TesteDoAvaliador {
+	public static void main(String[] args) {
+		Usuario joao = new Usuario("Joao");
+		Usuario maria = new Usuario("Maria");
+		Usuario jose = new Usuario("Jose");
+		
+		Leilao leilao = new Leilao("Playstation 3 novo");
+		
+		leilao.propoe(new Lance(joao, 300.0));
+		leilao.propoe(new Lance(maria, 400.0));
+		leilao.propoe(new Lance(jose, 250.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avaliar(leilao);
+		
+		System.out.println(leiloeiro.getMaiorLance());
+	}
+
+}
