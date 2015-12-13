2bc3af48a6112ca054155e8edb34d1cf5713c93e
Bruno Souza <brunoprogramadorjava>
2015-12-13 00:53:42 -0200

    Caso de teste com bug apos testar lances crescentes

--- src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -13,9 +13,9 @@ public class TesteDoAvaliador {
 		
 		Leilao leilao = new Leilao("Playstation 3 novo");
 		
-		leilao.propoe(new Lance(joao, 300.0));
-		leilao.propoe(new Lance(maria, 400.0));
-		leilao.propoe(new Lance(jose, 250.0));
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(maria, 300.0));
+		leilao.propoe(new Lance(jose, 400.0));
 		
 		Avaliador leiloeiro = new Avaliador();
 		leiloeiro.avaliar(leilao);
